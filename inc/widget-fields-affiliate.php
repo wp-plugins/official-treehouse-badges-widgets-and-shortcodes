38d1cc75dd34f26c4456249467218721ea4b5452
treehousezac <treehousezac@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-23 17:33:53 +0000

    Updated image URL for affiliates widget
    git-svn-id: https://plugins.svn.wordpress.org/official-treehouse-badges-widgets-and-shortcodes/trunk@1186362 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/widget-fields-affiliate.php
+++ inc/widget-fields-affiliate.php
@@ -2,7 +2,7 @@
   <label>Title</label> 
   <input class="widefat" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
 </p>
-<p><a href="http://referrals.trhou.se/<?php echo $options['wptreehouse_username']; ?>" target="_blank"><img class="wptreehouse-affiliate" src="http://wac.a8b5.edgecastcdn.net/80A8B5/static-assets/assets/content/referral-badge-green.png"/></a></p>
+<p><a href="http://referrals.trhou.se/<?php echo $options['wptreehouse_username']; ?>" target="_blank"><img class="wptreehouse-affiliate" src="https://static.teamtreehouse.com/assets/content/referral-badge-grn.png"/></a></p>
 <p><em>Enter in an option message to appear along with the affiliate image link.  Markup not allowed.</em></p>
 <p>
   <label>Custom Message</label> 
